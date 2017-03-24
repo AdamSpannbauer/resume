1cf9aceeac558a63392c3026054e3d9197ba3c94
Adam Spannbauer <SpannbauerAdam@gmail.com>
2017-03-23 21:49:53 -0400

    updates to graph for projects

--- helper_functions.R
+++ helper_functions.R
@@ -11,3 +11,28 @@ convert_dates <- function(yyyymm_col) {
     as.Date()
 }
 
+# Custom imputation function that interpolates missing values linearly and
+# replaces extreme NAs by the extreme values
+myImpute <- function(vec){
+  nonNAIds <- which(!is.na(vec))
+  if(length(nonNAIds)==0) return(vec)
+  
+  # Impute extreme values by replacing them with extreme non NA values
+  firstNonNA <- nonNAIds[1]
+  lastNonNA <- nonNAIds[length(nonNAIds)]
+  if(firstNonNA>1){
+    vec[1:firstNonNA] <- vec[firstNonNA]
+  }
+  n <- length(vec)
+  if(lastNonNA < n){
+    vec[lastNonNA:n] <- vec[lastNonNA]
+  }
+  
+  # Interpolate any remaining missing values linearly
+  if(any(is.na(vec))){
+    nonNAIds <- which(!is.na(vec))
+    vec <- approx(nonNAIds, y = vec[nonNAIds], xout = 1:n)$y
+  }
+  
+  vec
+}
--- resume_script.R
+++ resume_script.R
@@ -17,18 +17,32 @@ set.seed(42)
 resume_data <- read_csv("resume.csv",
                         col_types = "ccccclc") %>% 
   mutate(job_i = rank(start_date,"first")) %>% 
-  mutate(organization = as.factor(organization)) %>% 
-  mutate(organization = fct_reorder(organization, job_i)) %>% 
   mutate(start_date = convert_dates(start_date)) %>% 
   mutate(end_date   = convert_dates(end_date)) %>% 
   mutate(month = map2(start_date, end_date, ~seq(.x,.y,by="1 month"))) %>% 
   unnest() %>% 
   select(-start_date, -end_date) %>% 
-  mutate(data = .2*job_i+sample(seq(.1,.5,length.out=nrow(.)))) %>% 
+  # mutate(data = .2*job_i+sample(seq(.1,.5,length.out=nrow(.)))) %>% 
   nest(-month) %>% 
   mutate(month_i = row_number()) %>% 
-  unnest() %>% 
-  group_by(organization)
+  unnest()
+
+project_df <- read_csv("projects.csv", col_types = "ccccl") %>% 
+  mutate(date = convert_dates(date)) %>% 
+  rename(detail_proj = detail) %>% 
+  mutate(proj_i = rank(date,"first")) %>% 
+  right_join(resume_data %>% 
+              ungroup() %>% 
+              mutate(organization=as.character(organization)),
+            by=c("date"="month", "organization", "education")) %>% 
+  mutate(detail_proj = ifelse(detail_proj=="proprietary",
+                              "",
+                              detail_proj)) %>% 
+  mutate(proj_i = myImpute(proj_i)) %>%
+  mutate(data=proj_i + sample(seq(-.5,.5,length.out=nrow(.)))) %>% 
+  mutate(organization = as.factor(organization)) %>% 
+  mutate(organization = fct_reorder(organization, job_i))
+  
 
 colors <- resume_data %>% 
   group_by(organization,color) %>% 
@@ -41,6 +55,60 @@ time_labels <- resume_data %>%
   filter(str_sub(month, 6,7) == "01") %>%
   mutate(year=str_sub(month,1,4)) %>% 
   select(month_i, month, year)
+
+plot_ly(project_df,
+        x      = ~month_i,
+        y      = ~data,
+        color  = ~organization,
+        colors = colors,
+        type   = "scatter",
+        mode   = "lines",
+        line   = list(width=3),
+        hoverinfo = "text",
+        text = ~paste0(paste0(organization),
+                       "<br>",
+                       paste0(position),
+                       ifelse(is.na(detail),
+                              "",
+                              paste0("<br>",detail)))) %>% 
+  add_trace(project_df,
+            x     = ~month_i,
+            y     = ~proj_i,
+            showlegend = FALSE,
+            name  = "projects",
+            line = list(
+              color="black",
+              dash="dash"
+              ),
+            opacity=.3,
+            hoverinfo="none") %>% 
+  layout(title="",
+         xaxis=list(
+           title="",
+           tickmode="array",
+           tickvals=time_labels$month_i,
+           ticktext=time_labels$year,
+           tickangle = 45
+         ),
+         yaxis=list(
+           range = ~c(min(data)-2, 
+                      max(data)+2),
+           title = "",
+           zeroline = FALSE,
+           showline = FALSE,
+           showticklabels = FALSE,
+           showgrid = FALSE
+         ),
+         margin=list(b=75),
+         annotations=list(x = ~month_i[which(date==lexRankr_release)],
+                          y = ~data[which(date==lexRankr_release)],
+                          text = "lexRankr released on CRAN",
+                          xref = "x",
+                          yref = "y",
+                          showarrow = TRUE,
+                          arrowhead = 1,
+                          ax = -40,
+                          ay = -30))
 
 plot_ly(resume_data,
         x      = ~month_i,
@@ -83,6 +151,10 @@ plot_ly(resume_data,
                           showarrow = TRUE,
                           arrowhead = 1,
                           ax = -40,
-                          ay = -30))
+                          ay = -30)) %>% 
+  add_trace(x    = project_df$month_i,
+            y    = project_df$project_count,
+            type = "scatter",
+            mode = "lines")
 
 
